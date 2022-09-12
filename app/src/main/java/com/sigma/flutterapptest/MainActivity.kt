f8c66c7bb9524e361b203e36a2b5a388c32cec4c
Vishal Ahire <vjahirea3@gmail.com>
2022-09-12 23:04:14 +0530

    Implemented flutter in existing project

--- app/src/main/java/com/sigma/flutterapptest/MainActivity.kt
+++ app/src/main/java/com/sigma/flutterapptest/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.sigma.flutterapptest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnLaunch).setOnClickListener {
+            startActivity(
+//                FlutterActivity.createDefaultIntent(this)
+                FlutterActivity.withNewEngine().initialRoute("/my_route").build(this)
+            )
+        }
     }
 }
